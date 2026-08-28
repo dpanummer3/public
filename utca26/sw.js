6a5a52f5501a061f6876cc41ed17ecba20f2952d
dpanummer3 <david.alblas@gmail.com>
2026-08-28 16:47:38 +0200

    Add files via upload
    V8020

--- utca26/sw.js
+++ utca26/sw.js
@@ -1,5 +1,5 @@
-const CACHE_NAME='utca-shell-v5';
-const APP_SHELL=['/','/manifest.webmanifest?v=32','/icon-192.png?v=32','/icon-512.png?v=32'];
+const CACHE_NAME='utca-shell-v6';
+const APP_SHELL=['/','/manifest.webmanifest?v=39','/icon-192.png?v=39','/icon-512.png?v=39'];
 self.addEventListener('install',event=>{
 event.waitUntil(caches.open(CACHE_NAME).then(cache=>cache.addAll(APP_SHELL)).catch(()=>undefined));
 self.skipWaiting();
